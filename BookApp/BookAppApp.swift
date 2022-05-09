d7d3531950b1c8eaca462a30c488e312dcf5af2b
Gokhnan Bozkurt <gkhnbzkrt1000@gmail.com>
2022-05-09 19:32:21 +0300

    Initial Commit

---
+++ BookApp/BookAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  BookAppApp.swift
+//  BookApp
+//
+//  Created by Gokhan Bozkurt on 9.05.2022.
+//
+
+import SwiftUI
+
+@main
+struct BookAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
